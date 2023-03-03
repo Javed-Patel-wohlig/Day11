c24f49656ccbf999f4465226ae38a821c98c66bf
Javed-Patel-wohlig <javed.patel@wohlig.com>
2023-03-03 18:53:54 +0530

    aggregate done

--- Controller/Aggregate/lastEntry.js
+++ Controller/Aggregate/lastEntry.js
@@ -1,24 +1,80 @@
-const Customer = require("../../Models/customerSchema");
 const Order = require("../../Models/orderSchemas");
 const mongoose = require("../../Config/config");
 
 lastEntry = async (req, res) => {
-    try {
-        const pipeline =[
-            { $match: {  } },
-        ]
-        const result = await Customer.aggregate(pipeline);
-        const count = result.length;
-    
-        const populatedResult = await Customer.populate(result, { path: 'order' });
-    
-        res.status(200).send({
-          count: count,
-          data: populatedResult
-        });
-      } catch (err) {
-        res.status(500).send({ message: err.message });
-      }
-}
+  try {
+    const pipeline = [
+      // Group orders by customer
+      { $group: { _id: "$customer", latest_order: { $max: "$o_date_time" } } },
+      // Sort orders by date in descending order to get latest order
+      { $sort: { latest_order: -1 } },
+      // Limit results to 1 per customer
+      { $group: { _id: "$_id", latest_order: { $first: "$latest_order" } } },
+      // Lookup customer by _id
+      {
+        $lookup: {
+          from: "customers",
+          localField: "_id",
+          foreignField: "_id",
+          as: "customer",
+        },
+      },
+      // Unwind the customer array
+      { $unwind: "$customer" },
+      // Lookup the latest order by customer and o_date_time
+      {
+        $lookup: {
+          from: "orders",
+          let: { customer_id: "$_id", latest_order: "$latest_order" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ["$customer", "$$customer_id"] },
+                    { $eq: ["$o_date_time", "$$latest_order"] },
+                  ],
+                },
+              },
+            },
+          ],
+          as: "latest_order",
+        },
+      },
+      // Unwind the latest_order array
+      { $unwind: "$latest_order" },
+      // Project customer name and latest order fields
+      { $project: { name: "$customer.name", latest_order: "$latest_order" } },
+    ];
+
+    const pipeline2 = [
+      { $sort: { o_date_time: -1}},
+      { $group: { _id: "$customer", latest_order: {$first: '$$ROOT'} }},
+      { $lookup: 
+        { 
+          from:'customers',
+          localField: '_id',
+          foreignField: '_id',
+          as: 'user'
+         }
+      },
+      { $unwind: '$user' },
+      { $project: { name: '$user.name', latest_order: 1 } },
+      
+    ]
+
+    const result = await Order.aggregate(pipeline);
+    const count = result.length;
+
+    // const populatedResult = await Customer.populate(result, { path: 'order' });
+
+    res.status(200).send({
+      count: count,
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
 
 module.exports = lastEntry;
--- Models/customerSchema.js
+++ Models/customerSchema.js
@@ -44,7 +44,6 @@ const customerSchema = new mongoose.Schema({
       required: true,
   },
 
-  order: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }]
 },{timestamps: true});
 
 module.exports = mongoose.model('Customer', customerSchema);
